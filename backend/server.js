11f69bc8e50d390123d21e6b939ee545fcbaa09b
Kartik Bitla <bitlakartik@gmail.com>
2024-12-01 01:30:50 -0500

    Backend connected with Frontend

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ cloudinary.config({
 }); 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json()); //to parse req.body
