bbcd1e713e19adaaaf87745a4f9559b9e443ced9
Kartik Bitla <bitlakartik@gmail.com>
2024-12-23 15:58:06 +0530

    revering app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";  // Import Router here
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
@@ -12,50 +12,51 @@ import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {  
   const { data: authUser, isLoading } = useQuery({
-    queryKey: ['authUser'],
-    queryFn: async () => {
-      try {
+    //we use queryKey to give a unique name to our query and refer to it later
+    queryKey:['authUser'],
+    queryFn: async() => {
+      try{
         const res = await fetch("/api/auth/me");
         const data = await res.json();
-        if (data.error) {
+        if(data.error){
           return null;
         }
 
-        if (!res.ok) {
+        if(!res.ok){
           throw new Error(data.error || "Something went wrong");
         }
         console.log("authUser is here:", data);
         return data;
-      } catch (error) {
+      }catch(error){
         throw new Error(error);
       }
     },
     retry: false,
-  });
+  })
 
-  if (isLoading) {
-    return (
+
+
+  if(isLoading){
+    return(
       <div className="h-screen flex justify-center items-center">
-        <LoadingSpinner size="lg" />
+        <LoadingSpinner size="lg"/>
       </div>
-    );
+    )
   }
-
+  
   return (
-    <Router>
-      <div className="flex max-w-6xl mx-auto">
-        {authUser && <Sidebar />}
-        <Routes>
-          <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-          <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-          <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-          <Route path="/notifications" element={authUser ? <NotificationPage /> : <Navigate to="/login" />} />
-          <Route path="/profile/:userName" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-        </Routes>
-        {authUser && <RightPanel />}
-        <Toaster />
-      </div>
-    </Router>
+    <div className="flex max-w-6xl mx-auto">
+      {authUser && <Sidebar/>}
+      <Routes>
+        <Route path="/" element={authUser? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/login" element={!authUser? <LoginPage/> : <Navigate to="/" />} />
+        <Route path="/signup" element={!authUser? <SignUpPage/> : <Navigate to="/" />} />
+        <Route path="/notifications" element={authUser ? <NotificationPage/> : <Navigate to="/login"/>} />
+        <Route path="/profile/:userName" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>} />
+      </Routes>
+      {authUser && <RightPanel/>}    
+      <Toaster/>  
+    </div>
   );
 }
 
